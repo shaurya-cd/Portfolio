912e8473edb982c8fddb87c156ef0beee607d41f
shaurya <2447shaurya48@gmail.com>
2025-07-05 07:16:44 +0530

    Remove unused Router import from main.jsx and add vercel.json for URL rewrites

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import App from './App.jsx';
 import {BrowserRouter} from 'react-router-dom'
 import StoreContextProvider from './context/StoreContext.jsx'
